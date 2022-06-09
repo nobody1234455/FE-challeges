f413ab593bc90f18cc60830da667217c68809464
Thinh <quoc_thinh.nguyen@insa-cvl.fr>
2022-06-09 09:45:07 +0200

    rating project

---
+++ app.js
@@ -0,0 +1,6 @@
+function isChecked() {
+    const inputValue = document.querySelectorAll(".input input");
+    const action = document.querySelector("a.submit");
+    console.log(inputValue);
+    // action.href = "./thank-you.html";
+}
