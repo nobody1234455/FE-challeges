5f4cfc475067ecd6e017ba19651a381a214609df
Thinh <quoc_thinh.nguyen@insa-cvl.fr>
2022-06-09 10:51:14 +0200

    redirect to thank you page

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function isChecked() {
     const inputValue = document.querySelectorAll(".input input");
     const action = document.querySelector("a.submit");
-    console.log(inputValue);
-    // action.href = "./thank-you.html";
+    const n = inputValue.length;
+    action.href = "./thank-you.html";
 }
